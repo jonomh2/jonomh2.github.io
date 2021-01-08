c806b92c67d807766ba552107e6b2acc5fce8819
jonomh2 <jonathanm.hall10@gmail.com>
2021-01-08 16:41:15 +1000

    added project page

---
+++ app.js
@@ -0,0 +1,28 @@
+
+function requestUserRepos(){
+    const xhr = new XMLHttpRequest();
+    const url = `https://api.github.com/users/jonomh2/repos`;
+    xhr.open('GET', url, true);
+    xhr.onload = function () {
+        const data = JSON.parse(this.response);
+        for (let i in data) {
+            let ul = document.getElementById('userRepos');
+            let li = document.createElement('li');
+            li.classList.add('list-group-item')
+            li.innerHTML = (`
+                <p><strong> ${data[i].name}</strong></p>
+                <p>${data[i].description}</p>
+                <p> <a href="${data[i].html_url}">${data[i].html_url}</a></p>
+            `);
+            ul.appendChild(li);
+
+        }
+
+    }
+
+    // Send the request to the server
+    xhr.send();
+
+}
+
+requestUserRepos();
